48ba676ba0d4fc67bf0a883db1e7f37f1b29633d
Lenny Thobejane <Lennythobejane@gmail.com>
2024-06-05 10:18:40 +0200

    background image setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,32 @@
 // MUI
-
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+import Image from "next/image";
+
+// images
+import BackgroundImage from "../../public/background1.jpg";
+
 const LandingPage = () => {
 	return (
-		<Stack height={"100vh"} justifyContent={"center"} alignItems={"center"}>
+		<Stack
+			sx={{
+				position: "relative",
+				height: "100vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Image
+				src={BackgroundImage}
+				alt="Background Image"
+				layout="fill"
+				objectFit="cover"
+				quality={100}
+				style={{ position: "absolute", zIndex: -1, opacity: 0.3 }}
+			/>
 			<Typography variant={"h3"} sx={{ textAlign: "center" }}>
 				Welcome to Lenny's Todo Application
 			</Typography>
