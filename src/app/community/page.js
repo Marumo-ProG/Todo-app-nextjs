fc6497429c439981e856fcb11839a3ce8815b5d0
Lenny Thobejane <Lennythobejane@gmail.com>
2024-06-06 10:00:50 +0200

    cleanup

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -2,11 +2,6 @@
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
 
 // components
 import TodoCard from "../components/TodoCard";
